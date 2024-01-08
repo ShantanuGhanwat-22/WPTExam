e74f4f17e9b1907e9ac817e239b0d66cc8847e9a
SHANTANU GHANWAT <ghanwatshantanu2209@gmail.com>
2024-01-08 07:42:54 +0530

    ToDoList

--- src/App.js
+++ src/App.js
@@ -1,35 +1,39 @@
+// ToDoList
 import React, { useState } from 'react';
 import './App.css';
 
 function App() {
- const [list, setList] = useState([]);
- const [item, setItem] = useState('');
+ const [todos, setTodos] = useState([]);
+ const [input, setInput] = useState('');
 
- const addToList = () => {
-    setList([...list, item]);
-    setItem('');
+ const addTodo = (e) => {
+    e.preventDefault();
+    setTodos([...todos, input]);
+    setInput('');
  };
 
- const removeFromList = (index) => {
-    let newList = [...list];
-    newList.splice(index, 1);
-    setList(newList);
+ const deleteTodo = (index) => {
+    let newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
  };
 
  return (
     <div className="App">
-      <h1>Shopping List</h1>
-      <input 
-        type="text" 
-        value={item} 
-        onChange={(e) => setItem(e.target.value)}
-      />
-      <button onClick={addToList}>Add to List</button>
+      <h1>Todo List</h1>
+      <form onSubmit={addTodo}>
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button type="submit">Add</button>
+      </form>
       <ul>
-        {list.map((item, index) => (
+        {todos.map((todo, index) => (
           <li key={index}>
-            {item}
-            <button onClick={() => removeFromList(index)}>Delete</button>
+            {todo}
+            <button onClick={() => deleteTodo(index)}>Delete</button>
           </li>
         ))}
       </ul>
@@ -38,3 +42,65 @@ function App() {
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ********************************************************************************************************************************************************************************************************************************************************************************************************
+// Shopping List
+// import React, { useState } from 'react';
+// import './App.css';
+
+// function App() {
+//  const [list, setList] = useState([]);
+//  const [item, setItem] = useState('');
+
+//  const addToList = () => {
+//     setList([...list, item]);
+//     setItem('');
+//  };
+
+//  const removeFromList = (index) => {
+//     let newList = [...list];
+//     newList.splice(index, 1);
+//     setList(newList);
+//  };
+
+//  return (
+//     <div className="App">
+//       <h1>Shopping List</h1>
+//       <input 
+//         type="text" 
+//         value={item} 
+//         onChange={(e) => setItem(e.target.value)}
+//       />
+//       <button onClick={addToList}>Add to List</button>
+//       <ul>
+//         {list.map((item, index) => (
+//           <li key={index}>
+//             {item}
+//             <button onClick={() => removeFromList(index)}>Delete</button>
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//  );
+// }
+
+// export default App;
